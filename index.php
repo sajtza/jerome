110a2e51d2b597e57b8454671030d799d40ecd0e
sajtza <128223744+sajtza@users.noreply.github.com>
2023-03-19 12:39:36 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,437 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    <!-- custom css -->
+<link rel="stylesheet" href="css/style.css">
+     <!-- bootstrap icons -->
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
+    <title>Jerome | site</title>
+
+
+</head>
+<body>
+    <!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+    <a href="#" class="navbar-brand">JEROME</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+    data-bs-target="#navmenu">
+<span class="navbar-toggler-icon"></span></button>
+    <div class="collapse navbar-collapse" id="navmenu">
+        <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <a href="#home" class="nav-link">Home</a>
+        </li>
+
+
+        <li class="nav-item">
+            <a href="#aboutme" class="nav-link"
+            >About Me</a>
+        </li>
+
+        <li class="nav-item">
+            <a href="#services" class="nav-link "
+            
+            >Services</a>
+        </li>
+        <li class="nav-item" >
+            <a href="#contacts" class="nav-link "
+                    >Contacts</a>
+        </li>
+        <li class="nav-item">
+            <a href="#faq" class="nav-link "
+            >FAQs</a>
+        </li>
+    </ul>
+   </div>
+   </div>
+
+</nav>
+
+
+<!-- showcase -->
+<section id="home" class="pt-5">
+<div class="bg-dark text-center text-light py-3" id="Home">
+    <div class="container">
+        <div class="row pb-3">
+            <div class="col-md pt-4">
+              <h3><span class="highlights">Welcome t</span>o my site</h3>
+              <p class="card-text pt-3">Hi! I am Jerome here, a <span class="text-primary fw-bolder">web developer. </span> Let me take this chance first to
+                thank you for your decision to visit my site. I'm interested to 
+                work with anyone in the issues related to web application development, website-maintenance and other issues related to web applications.
+              </p>
+              <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#notifyme"> Email Me</button> 
+            </div>
+            <div class="col-md d-none d-lg-block">
+<img src="img/img1.svg" class="img-fluid" alt="" srcset="">
+        </div>
+        </div>
+    </div>
+</div>
+</section>
+
+<!-- ABOUT ME -->
+<section id="aboutme" class=" text-center my-1">
+    <div class="container">
+      <h4 class="fw-bolder py-4"> <span class="highlights">Abou</span>t Me</h4>
+    <div class="row">
+        <div class="col-md-7">
+            <div class="card mb-3 card-change">
+                <div class="row g-0">
+                  <div class="col-md-4">
+                    <img src="img/img2.jpg" class="avartar img-fluid rounded-circle" alt="Jerome photo">
+                  </div>
+                  <div class="col-md-8">
+                    <div class="card-body">
+                      <h5 class="card-title"><span class="highlights">Perso</span>nal details</h5>
+                      <p class="card-text text-md-end ">
+                        "My name is <strong class="text-primary ">Jerome, Samwel Aloyce</strong> a graduate from Mzumbe university
+                        with a bachelor degree in mathematics and ICT interested in 
+                        web application development, I have a strong experience in this 
+                        field, I have been developing web applications since 2021, I'm ready to work
+                        with any business organisation on the issues related to this industry
+                     you won't regret my service"
+                      </p>
+                      <p class="card-text"><small class="text-muted">You are warmly welcome......</small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+
+
+     
+        <div class="col-md-5 ">
+            <div class="card text-center card-change">
+                <div class="card-body">
+                  
+                    <h5 class="card-title pb-2"><span class="highlights">Techn</span>ologies
+                    </h5>
+        
+
+                    <ul class="">
+                        <li class=" list-group-item ">
+                            <div class="row pt-1 gx-1">
+                                <div class="col-md-4">
+                                    <p class="text-start card-text">HTML</p>
+                                </div>
+                           <div class="col-md-8 pt-1">
+                                <div class="col-10 progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar" style="width: 95%">95%</div>
+                    </div>
+                    </div>
+                    
+                    
+                          
+                        </li>
+
+                        <li class=" list-group-item ">
+                            <div class="row pt-1 gx-1">
+                                <div class="col-md-4">
+                                    <p class="text-start card-text">CSS</p>
+                                </div>
+                           <div class="col-md-8 pt-1">
+                                <div class="col-10 progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar" style="width: 75%">75%</div>
+                    </div>
+                    </div>
+                
+                    
+                          
+                        </li>
+
+                        <li class=" list-group-item ">
+                            <div class="row pt-1 gx-1">
+                                <div class="col-md-4">
+                                    <p class="text-start card-text">JavaScript</p>
+                                </div>
+                           <div class="col-md-8 pt-1">
+                                <div class="col-10 progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar" style="width: 70%">70%</div>
+                    </div>
+                    </div>
+                    
+                          
+                        </li>
+
+                        <li class=" list-group-item ">
+                            <div class="row pt-1 gx-1">
+                                <div class="col-md-4">
+                                    <p class="text-start card-text">BOOTSTRAP</p>
+                                </div>
+                           <div class="col-md-8 pt-1">
+                                <div class="col-10 progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar" style="width: 65%">65%</div>
+                    </div>
+                    </div>
+                    
+                    
+                          
+                        </li>
+
+                        <li class=" list-group-item ">
+                            <div class="row pt-1 gx-1">
+                                <div class="col-md-4">
+                                    <p class="text-start card-text">PhP</p>
+                                </div>
+                           <div class="col-md-8 pt-1">
+                                <div class="col-10 progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar" style="width: 80%">80%</div>
+                    </div>
+                    </div>
+                                
+                          
+                        </li>
+
+                        <li class=" list-group-item ">
+                            <div class="row pt-1 gx-1">
+                                <div class="col-md-4">
+                                    <p class="text-start card-text">LARAVEL</p>
+                                </div>
+                           <div class="col-md-8 pt-1">
+                                <div class="col-10 progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                      <div class="progress-bar" style="width: 60%">60%</div>
+                    </div>
+                    </div>
+                                 
+                          
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- PROJECTS -->
+<section id="services" class="bg-dark text-light text-center mt-5 my-1">
+    <div class="container">
+        <h4 class="fw-bolder pt-3"> <span class="highlights">My services fe</span>atures</h4>
+    <div class="row">
+
+      <div class="col-md-4">
+        <div class="card mb-3 bg-dark text-light card-change">
+                <div class="card-body">
+                  <i class="bi bi-phone pb-2" style="font-size:60px; color:aqua"></i>
+                  <h5 class="card-title lead">Responsiveness</h5>
+                  <p class="card-text">
+           The site that I develop must fit to any devices be to
+           a computer, tablet and smartphone, or other devices.
+                  </p>
+              </div>
+            </div> 
+    </div>
+        <div class="col-md-4">
+            <div class="card mb-3 bg-dark text-light card-change">
+                    <div class="card-body">
+                      <i class="bi bi-clock pb-2" style="font-size:60px; color:aqua"></i>
+                      <h5 class="card-title lead">24/7 Available supports</h5>
+                      <p class="card-text">
+                The services is available throughout a day, once you will
+                be in need for my services feel free anytime the services 
+                is available.
+                      </p>
+      
+                  </div>
+                </div> 
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-3 bg-dark text-light card-change">
+                  <div class="card-body">
+                    <i class="bi bi-people pb-2" style="font-size:60px; color:aqua"></i>
+                    <h5 class="card-title lead">Good customer care</h5>
+                    <p class="card-text">
+         The customer needs and requirements are highly 
+         prioritized to ensure a customer get satisfied.
+                    </p>
+              
+                </div>
+              </div> 
+      </div>
+
+
+      <div class="col-md-4">
+        <div class="card mb-3 bg-dark text-light card-change">
+                <div class="card-body">
+                  <i class="bi bi-search pb-2" style="font-size:60px; color:aqua"></i>
+                  <h5 class="card-title lead">Easily Accessible</h5>
+                  <p class="card-text">
+      The websites and web application developed are easily 
+      accessible by anyone from anywhere.
+                  </p>
+            
+              </div>
+            </div> 
+    </div>
+
+    <div class="col-md-4">
+      <div class="card mb-3 bg-dark text-light card-change">
+              <div class="card-body">
+                <i class="bi bi-lock pb-2" style="font-size:60px; color:aqua"></i>
+                <h5 class="card-title lead">Strong Security</h5>
+                <p class="card-text">
+  The application or website developed are of good standard
+  in all aspects including security.
+                </p>
+          
+            </div>
+          </div> 
+  </div>
+
+  <div class="col-md-4">
+    <div class="card mb-3 bg-dark text-light card-change">
+            <div class="card-body">
+              <i class="bi bi-gear pb-2" style="font-size:60px; color:aqua"></i>
+              <h5 class="card-title lead">Maintenance/Updation</h5>
+              <p class="card-text">
+The application will be maintaned wherever and whenever 
+it seem there is any security threat.
+              </p>
+        
+          </div>
+        </div> 
+</div>
+
+
+    </div>
+    
+</section>
+
+
+<section id="contacts" class="mt-5 ">
+<h4 class="py-3 text-center"><span class="highlights">Conta</span>cts</h4>
+    <div class="container">
+
+        <div class="row ">
+            <div class="col-md-6 pb-3">
+                <p class="lead text-start">Contact with me via</p>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item single"><i class="bi bi-envelope-check text-primary text-satrt"></i> <span class="float-end">samwelaloyce10@gmail.com      </span></li>
+                    <li class="list-group-item"><i class="bi bi-twitter text-primary text-satrt"></i> <span class="float-end">@jerome_tza      </span></li>
+                    <li class="list-group-item"><i class="bi bi-instagram text-primary text-satrt"></i> <span class="float-end">@jerome_tech     </span></li>
+                    <li class="list-group-item"><i class="bi bi-facebook text-primary text-satrt"></i> <span class="float-end">Sam A Jerome    </span></li>
+                    <li class="list-group-item"><i class="bi bi-whatsapp text-primary text-satrt"></i> <span class="float-end">+255 628 426 671     </span></li>
+
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <p class="lead text-start">My location</p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31884.05588871822!2d31.3312859!3d-2.6638271!3m2!1i1024!2i768!4f13.1!4m3!3e6!4m0!4m0!5e0!3m2!1sen!2stz!4v1679116517621!5m2!1sen!2stz" width="100%" height="80%" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+    <!-- FAQ -->
+    <section id="faq" class="my-5">
+        <div class="container">
+          <h4 class="fw-bolder text-center py-4"> <span class="highlights">Frequentl</span>y Asked Questions</h4>
+            <div class="accordion" id="accordionExample">
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                How can I access your service?
+                    </button>
+                  </h2>
+                  <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        Well, you can access my service simply by notifying me via the form or contacts above,
+                        then we shall communicate and make some arrangements on how we can accomplish your
+                        desired projects.
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    How payment is made ?
+                    </button>
+                  </h2>
+                  <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                      Before the beginning of development process, a client will be required to pay 
+                      at least 30% of the project's financial agreement, the remains one will be paid as 
+                      the project will be in the progress.
+                  </div>
+                </div>
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                      At what time-interval a project can be accoplished?
+                    </button>
+                  </h2>
+                  <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                      It depend with the project's requirement but for a simple project like a website, web application with
+                      few requirements it can consume almost one week but for the huge projects like those 
+                      which need the integrations of many APIs need more time more than one week.
+                    </div>
+                  </div>
+                </div>
+              </div>
+        </div>
+    </section>
+
+<!-- Modal for getting touch with me -->
+<div class="modal fade" id="notifyme" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5 lead align-items-center" id="staticBackdropLabel">Fill the information below</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form action="index.php" method="post">
+        <div class="modal-body">
+          <div class="form-group mb-3">
+            <input type="text" name="name" id="" class="form-control" placeholder="Your Name">
+        </div>
+            <div class="form-group mb-3 ">
+                <input type="email" name="email" id="" class="form-control" placeholder="Your Email">
+            </div>
+            
+            <div class="form-group mb-3">
+              <input type="text" name="subject" id="" class="form-control" placeholder="Subject">
+          </div>
+           
+            <div class="form-group mb-3">
+                <textarea name="message" id="" cols="30" rows="5" class="form-control" placeholder="Message"></textarea>
+            </div>
+     
+        </div>
+        <div class="modal-footer">
+          
+          <input type="submit" class="btn btn-primary"  value="send message">
+        </div>
+      </div>
+    </form>
+    </div>
+  </div>
+
+
+
+<footer class=" bg-dark text-light position-relative p-4">
+    <div class="container">
+        <p class=" text-center text-white">Copyright&copy;Jerome 2023</p>
+<a href="#" class="position-absolute bottom-0 end-0 p-4 h3 text-danger">
+    <i class="bi bi-arrow-up-circle "></i>
+</a>
+    </div>
+</footer>
+
+
+
+    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
